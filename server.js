7b3f48ae3bb7682922b5b7015ead7b531ab65415
johnrobertlapuz <j.robert.p.lapuz@gmail.com>
2021-05-05 22:39:15 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('./models/db');
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Listening to PORT ${port}. Go to http://localhost:${port}/employee`);
+});
+app.use('/employee', employeeController);
